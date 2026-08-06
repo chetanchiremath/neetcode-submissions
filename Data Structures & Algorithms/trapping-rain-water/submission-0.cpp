7a6bc9663ae36172c327171c059e0405bc4dee5b
Chetan C Hiremath <137285002+chetanchiremath@users.noreply.github.com>
2026-08-06 21:06:28 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int l = 0, r = n-1;
+        int lmax = 0, rmax = 0;
+        int res = 0;
+        while(l < r) {
+            if(height[l] <= height[r]) {
+                if(lmax > height[l]) res += lmax - height[l];
+                else lmax = height[l];
+                l++;
+            }
+            else {
+                if(rmax > height[r]) res += rmax - height[r];
+                else rmax = height[r];
+                r--;
+            }
+        }
+        return res;
+    }
+};
